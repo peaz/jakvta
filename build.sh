20ffc3c5e34d772d8deb6ad6ac6c15a9f00f9ad3
Ken Ng <ken@sterlingpj.com>
2024-10-19 10:21:55 +0800

    arm and x64 releases

--- build.sh
+++ build.sh
@@ -12,6 +12,9 @@ else
     echo "Directory already exists: $directory"
 fi
 
-env GOOS=linux GOARCH=amd64 go build -o="$directory/jakvta-$version-linux" $PWD/src/jakvta/main.go
-env GOOS=darwin GOARCH=amd64 go build -o="$directory/jakvta-$version-osx" $PWD/src/jakvta/main.go
-env GOOS=windows GOARCH=amd64 go build -o="$directory/jakvta-$version-win.exe" $PWD/src/jakvta/main.go
+env GOOS=linux GOARCH=amd64 go build -o="$directory/jakvta-$version-linux-x64" $PWD/src/jakvta/main.go
+env GOOS=darwin GOARCH=amd64 go build -o="$directory/jakvta-$version-osx-x64" $PWD/src/jakvta/main.go
+env GOOS=windows GOARCH=amd64 go build -o="$directory/jakvta-$version-win-x64.exe" $PWD/src/jakvta/main.go
+env GOOS=linux GOARCH=arm64 go build -o="$directory/jakvta-$version-linux-arm" $PWD/src/jakvta/main.go
+env GOOS=darwin GOARCH=arm64 go build -o="$directory/jakvta-$version-osx-arm" $PWD/src/jakvta/main.go
+env GOOS=windows GOARCH=arm64 go build -o="$directory/jakvta-$version-win-arm.exe" $PWD/src/jakvta/main.go
